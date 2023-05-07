a1add322de43e6c5b37d156f546ecd1f2224b91e
Tagne237 <100275512+Tagne237@users.noreply.github.com>
2023-05-07 16:58:51 +0100

    add edit file

--- src/app/views/view-doc/view-doc.page.ts
+++ src/app/views/view-doc/view-doc.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ViewChild, OnInit  } from '@angular/core';
+import { IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-view-doc',
@@ -12,4 +15,23 @@ export class ViewDocPage implements OnInit {
   ngOnInit() {
   }
 
+  @ViewChild(IonModal) modal !: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  dette!: string;
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.dette, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 }
