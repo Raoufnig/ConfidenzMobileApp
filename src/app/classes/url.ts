bcd6fd26ea21dccf5f3d80c40e2a49a40aad916b
fotsoclaude <fotso1claude@gmail.com>
2023-06-07 10:19:35 +0100

    Infos update

--- src/app/classes/url.ts
+++ src/app/classes/url.ts
@@ -2,7 +2,7 @@ export class URL {
     static readonly API_VERSION = 'v1';
     // static readonly BASE_URL = 'http://localhost:8000';
     static readonly BASE_URL = 'https://conf-api.fotsoclaude.com';
-    static readonly API_URL = this.BASE_URL + '/api/'+ this.API_VERSION;
+    static readonly API_URL = this.BASE_URL + '/api/' + this.API_VERSION;
     static readonly COMPAGNY_URL = this.API_URL + '/compagny';
     static readonly EMPLOYEE_URL = this.API_URL + '/employee';
 }
--- src/app/views/home/home.page.ts
+++ src/app/views/home/home.page.ts
@@ -9,54 +9,54 @@ import { URL } from 'src/app/classes/url';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-   util : any;
-   utilInfo : any;
-   fichiers: any
-   storeData:any;
-   loaded:boolean = false;
-   docs:any;
-   filteredData: any[] = [];
-   searchText!: string;
-  constructor(private router:Router , private loadingCtrl : LoadingController) { }
+  util: any;
+  utilInfo: any;
+  fichiers: any
+  storeData: any;
+  loaded: boolean = false;
+  docs: any;
+  filteredData: any[] = [];
+  searchText!: string;
+  constructor(private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
     this.util = localStorage.getItem('userInfo');
-    this.utilInfo =JSON.parse(this.util); 
+    this.utilInfo = JSON.parse(this.util);
     console.log(this.utilInfo)
     this.listDocEmployee();
   }
 
-  toPage(){
+  toPage() {
     this.router.navigate(['/detail-doc'])
   }
-  listDocEmployee(){
+  listDocEmployee() {
     this.loaded = false
-    
-    axios.get(URL.EMPLOYEE_URL +'/files',{
-      headers:{
-        'Authorization': 'Bearer '+ this.utilInfo.authorization.token ,
+
+    axios.get(URL.EMPLOYEE_URL + '/files', {
+      headers: {
+        'Authorization': 'Bearer ' + this.utilInfo.authorization.token,
       }
-    }).then((response)=>{
-      
-      
-      this.docs=response.data.files;
-      this.filteredData=response.data.files;
+    }).then((response) => {
+
+
+      this.docs = response.data.files;
+      this.filteredData = response.data.files;
       console.log(this.docs)
       console.log(response)
       this.loaded = true;
-      
-    
-    }).catch((error)=>{
+
+
+    }).catch((error) => {
       console.log(error)
     })
-    
+
   }
 
-  onSearchChange(ev : any){
+  onSearchChange(ev: any) {
     const value = ev.target.value.toLowerCase()
-    
-      this.filteredData = this.filteredData.filter((d) => d.name.toLowerCase().indexOf(value) > -1);
-    
+
+    this.filteredData = this.filteredData.filter((d) => d.name.toLowerCase().indexOf(value) > -1);
+
   }
 
 
@@ -64,13 +64,13 @@ export class HomePage implements OnInit {
     localStorage.removeItem('Doc');
     localStorage.removeItem('count');
     localStorage.removeItem('firstvisite');
-    localStorage.setItem('firstvisite','firstvisite')
-    localStorage.setItem('Doc',JSON.stringify(doc));
-    localStorage.setItem('count',JSON.stringify(doc.heading_level))
+    localStorage.setItem('firstvisite', 'firstvisite')
+    localStorage.setItem('Doc', JSON.stringify(doc));
+    localStorage.setItem('count', JSON.stringify(doc.heading_level))
     this.router.navigateByUrl(`tab/home/detail-doc/${doc.id}`);
   }
-  
-  handleRefresh(event : any) {
+
+  handleRefresh(event: any) {
     setTimeout(() => {
       // Any calls to load data go here
       window.location.reload();
