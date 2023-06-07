de9febe0ce7fd85074e7c1f86f65a456bc8921bc
fotsoclaude <fotso1claude@gmail.com>
2023-06-07 10:04:57 +0100

    ui update

--- src/app/views/login/login.page.ts
+++ src/app/views/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {FormGroup , FormBuilder , Validators} from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/Services/auth.service';
 @Component({
   selector: 'app-login',
@@ -11,39 +11,41 @@ export class LoginPage implements OnInit {
   showPassword: boolean = false;
   error: any;
   user: any;
-  isConnected =true;
-  loader:Boolean=false;
+  isConnected = true;
+  loader: Boolean = false;
 
   loginForm: FormGroup;
-  constructor(private formBuilder: FormBuilder,private router:Router, private authservice : AuthService ) {
+  constructor(private formBuilder: FormBuilder, private router: Router, private authservice: AuthService) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
-    
+
   }
 
   ngOnInit() {
-    
+
   }
   async onSubmit() {
     if (this.loginForm.invalid) {
       console.log("invalide");
       return;
     }
-    else{
-      this.loader=true;
-      let result={ email: this.loginForm.value.email, password: this.loginForm.value.password}
-      console.log("valide");
-      console.log(result);
-      await this.authservice.login(result).then(()=>{
-        this.loader=false;
-        this.router.navigate(['tab/home'])
-      });
-      
-    }
+
+    this.loader = true;
+    let result = { email: this.loginForm.value.email, password: this.loginForm.value.password }
+    console.log("valide");
+    console.log(result);
+
+    await this.authservice.login(result).then((response: any) => {
+      this.loader = false;
+      localStorage.clear();
+      localStorage.setItem('userInfo', JSON.parse(response.data))
+      this.router.navigate(['tab/home'])
+    });
+
 
     // handle form submission
   }
-  
+
 }
